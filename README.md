f5b5a2a031e4826876746d4f253b9982e399fa80
kgarbacinski <kacpergarbacinski@gmail.com>
2025-02-20 11:05:19 +0100

    [DEPLOYMENT] Add deploying scripts for tests & prod.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Deploy
+
+```
+source .env
+forge script scripts/prod/01_DeployTrainingsManager.sol --rpc-url $RPC_URL --private-key $PRIVATE_KEY --etherscan-api-key $ETHERSCAN_API_KEY --broadcast --verify
+```
---
+++ scripts/prod/01_DeployTrainingsManager.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+import "forge-std/Script.sol";
+import {ITrainingsManager} from "src/trainings/ITrainingsManager.sol";
+import {TrainingsManager} from "src/trainings/TrainingsManager.sol";
+
+
+contract DeployTrainingsManager is Script {
+    function run() public {
+        vm.startBroadcast();
+        deployTrainingsManager();
+        vm.stopBroadcast();
+    }
+
+    function deployTrainingsManager() internal {
+        console.log("Deploying TrainingsManager...");
+
+        ITrainingsManager trainingsManager = new TrainingsManager();
+
+        console.log("TrainingsManager deployed at:", address(trainingsManager));
+    }
+}
