a23d0b96de7d91f680bba6a6fd5e7a1a0c2a83f4
kgarbacinski <kacpergarbacinski@gmail.com>
2025-03-13 12:32:03 +0100

    feat: add proxy compliant with eip-1967 & eip-173

---
+++ src/intervals/IIntervals.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+contract IIntervals {
+    /// @notice This is an interface for contract that represents time intervals which are created rankings for with rewards.
+
+    constructor() {}
+}
